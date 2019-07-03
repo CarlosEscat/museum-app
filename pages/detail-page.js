643a940de65eac2e47294fe8c82924fcc5104a75
CarlosEscat <carlosescat@gmail.com>
2019-07-03 19:49:08 +0200

    Added two extra validations

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -3,48 +3,45 @@ function submitComment(){
     const name = inputField.value
     const textArea = document.getElementById('msg')
     const msg = textArea.value
-
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
 
-    h3.innerHTML = `${name} said:`
+    if(doesNotPassAllValidations(name, msg)){
+        return null
+    }
+
+    h3.innerHTML = `${name[0].toUpperCase() + name.slice(1)} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
     
-    if(doesNotPassAllValidations(name, msg)){
-        return null
-    }
-
     const commentSection = document.getElementById('comments')
     commentSection.appendChild(comment)
     
     inputField.value = null
     textArea.value = null
-
-    if (!name || !msg){
-        alert('You forgot to fill in your name or message!')
-        return true
-    } 
-
-    
 }
 
 function doesNotPassAllValidations(name, msg) {
-    if (!name || !msg) {
+    if (!name && !msg) {
       alert('You forgot to fill in your name or message!')
       return true;
+    } else if (!name) {
+        alert('You forgot to fill in your name!')
+        return true;
+    } else if (!msg){
+        alert('You forgot to fill in your message!')
+        return true;
+    } else {
+        return false
     }
-  
+
     if(msg.length > 280) {
       alert('Your comment is too long')
       return true
     }
-  
+
     return false
   }
-
-
-
