2aa8ca25d2e5e6fe258ceb60d48179aa6e64e71e
CarlosEscat <carlosescat@gmail.com>
2019-07-03 17:57:56 +0200

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -3,16 +3,48 @@ function submitComment(){
     const name = inputField.value
     const textArea = document.getElementById('msg')
     const msg = textArea.value
+
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
-    const commentSection = document.getElementById('comments')
+
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
-    commentSection.appendChild(comment)
+    
+    if(doesNotPassAllValidations(name, msg)){
+        return null
+    }
 
-    console.log(commentSection)
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+    
+    inputField.value = null
+    textArea.value = null
+
+    if (!name || !msg){
+        alert('You forgot to fill in your name or message!')
+        return true
+    } 
+
+    
 }
+
+function doesNotPassAllValidations(name, msg) {
+    if (!name || !msg) {
+      alert('You forgot to fill in your name or message!')
+      return true;
+    }
+  
+    if(msg.length > 280) {
+      alert('Your comment is too long')
+      return true
+    }
+  
+    return false
+  }
+
+
+
